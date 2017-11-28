60274ff796f0c45117fad714be00222e4331bc25
Rob Cho <robhcho@gmail.com>
2017-11-27 20:59:36 -0500

    Show title of articles to user

--- reddit_app.rb
+++ reddit_app.rb
@@ -1,7 +1,22 @@
 require "unirest"
 
-response = Unirest.get("https://www.reddit.com/r/movies/.json")
-reddit_data = response.body
-# p reddit_data
+response = Unirest.get("https://www.reddit.com/r/ProGolf/.json")
+# subreddit_data = response.body
+# p subreddit_data
 
-comments = 
+# p "Please enter the number of the post you would like to view"
+# i = gets.chomp.to_i
+
+# i = 0
+
+# title_of_post = subreddit_data["data"]["children"][i]["data"]["title"]
+# p title_of_post
+
+# comments = subreddit_data['data']['children'][i]['data']['permalink']
+# p comments
+
+articles = response.body["data"]["children"]
+
+articles.each do | article |
+  p article["data"]["title"]
+end
